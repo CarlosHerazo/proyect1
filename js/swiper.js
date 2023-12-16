b9adfd0fb021ae0a43f3ea1c37462ae80bb97817
CarlosHerazo <Carlosherazo2004@gmail.com>
2023-12-16 18:47:59 -0500

    Cambio en el index

--- js/swiper.js
+++ js/swiper.js
@@ -2,29 +2,32 @@ const swiper = new Swiper('.mySwiper', {
     // Opciones de Swiper
     slidesPerView: 4,
     spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
     // Habilitar autoplay
     autoplay: {
         delay: 3000, // Retraso de 3 segundos
         disableOnInteraction: false, // Continuar con autoplay después de la interacción del usuario
     },
-});
-
-
-const swipers = new Swiper('.mySwipers', {
-    // Opciones de Swiper
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    // Habilitar autoplay
-    autoplay: {
-        delay: 3000, // Retraso de 3 segundos
-        disableOnInteraction: false, // Continuar con autoplay después de la interacción del usuario
-    },
+    // Breakpoints
+    breakpoints: {
+        // Cuando la anchura de la ventana es >= 500px 
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // Cuando la anchura de la ventana es >=600px 
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // Cuando la anchura de la ventana es >= 768px 
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // Cuando la anchura de la ventana es >= 1080px 
+        1080: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
 });
