e3d1c8cdb39972eabe14764cf3ad64c410f5a303
CarlosHerazo <Carlosherazo2004@gmail.com>
2023-12-16 18:04:26 -0500

    Terminando  el index

--- js/swiper.js
+++ js/swiper.js
@@ -12,3 +12,19 @@ const swiper = new Swiper('.mySwiper', {
         disableOnInteraction: false, // Continuar con autoplay después de la interacción del usuario
     },
 });
+
+
+const swipers = new Swiper('.mySwipers', {
+    // Opciones de Swiper
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // Habilitar autoplay
+    autoplay: {
+        delay: 3000, // Retraso de 3 segundos
+        disableOnInteraction: false, // Continuar con autoplay después de la interacción del usuario
+    },
+});
